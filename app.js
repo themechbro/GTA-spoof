317222c95345c8e543f1bd80cc69367940a34dfe
Adrin T Paul <109350438+themechbro@users.noreply.github.com>
2023-10-28 14:55:36 +0530

    version 1.1

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ gsap.from(['.navbar-brand','.nav-item'],{y:5, opacity:0, duration:1, delay:1, ea
 gsap.from('.first',{scrollTrigger:'.first', x:-200, duration:1, delay:1, opacity:0, ease:"sine.out",stagger:1})
 gsap.from('.second',{scrollTrigger:'.second', x:200, duration:1, delay:1, opacity:0, ease:"sine.out",stagger:1})
 gsap.from('.third-frame > .container> .row >.col-12',{scrollTrigger:'.third-frame > .container', opacity:0, delay:1, duration:1, ease:"sine.out", stagger:0.5})
+gsap.from('footer',{scrollTrigger:'footer', opacity:0, delay:0.2, duration:1, ease:"sine.out"} )
+gsap.from(['.first-footer','.second-footer','.third-footer'],{scrollTrigger:'footer', y:10, delay:0.5, duration:1, ease:"back.out"} )
